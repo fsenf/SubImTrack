d9f8be2b02aef5a54518a6d0ed215c19da1dfed4
Fabian Senf <senf@tropos.de>
2020-04-28 17:24:55 +0200

    forgot setup.py file...

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='subimtrack',
+      use_scm_version=True,
+      setup_requires=['setuptools_scm'],
+      description='Subjective Image Tracking',
+      url='http://github.com/fsenf/SubImTrack',
+      author='Fabian Senf',
+      author_email='senf@tropos.de',
+      license='BSD-3',
+      packages=[],
+      include_package_data=True,
+      install_requires=[],
+      zip_safe=False)
